f8045ae631879f89b43f27593ae0f9d59542f8ee
Fabian58tz <fhernandez117_est@instipp.edu.ec>
2025-12-04 01:20:31 -0500

    corresion

--- trelles_dental/settings.py
+++ trelles_dental/settings.py
@@ -16,8 +16,8 @@ load_dotenv(BASE_DIR / ".env") # Cargar variables de entorno desde el archivo .e
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-^*a!lecnh3^oqz)9ddfd((35x39vvp6mou3gmplh!c$xti_$91')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get('DEBUG_VALUE', 'False') == 'True'
+DEBUG = True
+#DEBUG = os.environ.get('DEBUG_VALUE', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
