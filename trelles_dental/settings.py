e8e958f3e5cff5cb87f6cddc728010884c7b1e6a
Fabian58tz <fhernandez117_est@instipp.edu.ec>
2025-12-03 21:34:37 -0500

    agregado

--- trelles_dental/settings.py
+++ trelles_dental/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    STATIC_DIR,    
+    os.path.join(BASE_DIR, 'static'),  
 ]
 
 
